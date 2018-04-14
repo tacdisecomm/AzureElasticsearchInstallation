1796fc1ed16a88df2c6f563907629aaa3f7ecc19
Pierre setteskog <pierre.setteskog@wipcore.se>
2018-04-14 11:49:27 +0200

    f

--- elasticsearch-windows-install.ps1
+++ elasticsearch-windows-install.ps1
@@ -344,7 +344,7 @@ function ElasticSearch-InstallService($scriptPath)
         SetEnv-HeapSize
 
         lmsg 'Installing elasticsearch as a service...'
-        cmd.exe /C "set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_162 & $scriptPath install"
+        cmd.exe /C "$scriptPath install"
         if ($LASTEXITCODE) {
             throw "Command '$scriptPath': exit code: $LASTEXITCODE"
         }
@@ -517,9 +517,9 @@ function Install-WorkFlow
     lmsg "install jdk8 start"
     choco install jdk8 -y
     lmsg "install jdk8 end"
-    lmsg "sleep start"
-    Start-Sleep 300
-    lmsg "sleep end"
+#    lmsg "sleep start"
+#    Start-Sleep 300
+#    lmsg "sleep end"
     #setx /M JAVA_HOME "C:\Program Files\Java\jdk1.8.0_162"
 #	$jdkSource = Download-Jdk $firstDrive
 	
@@ -597,8 +597,8 @@ function Install-WorkFlow
         $textToAppend = $textToAppend + "`nnetwork.host: $nodeEndpoint"
     }
     lmsg 'install pplugin start'
-	cmd.exe /C "set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_162 & $elasticSearchBin\plugin.bat install mobz/elasticsearch-head -b"
-	cmd.exe /C "set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_162 & $elasticSearchBin\plugin.bat install appbaseio/dejavu -b"
+	cmd.exe /C "$elasticSearchBin\plugin.bat install mobz/elasticsearch-head -b"
+	cmd.exe /C "$elasticSearchBin\plugin.bat install appbaseio/dejavu -b"
     lmsg 'install pplugin end'
 	# configure the cloud-azure plugin, if selected
 	if ($po.Length -ne 0 -and $r.Length -ne 0)
